8122ffb7cd1205f2d6227442acefeb2312a674ec
nikita <nikita.parekh@unboxrobotics.com>
2023-06-18 00:25:55 +0530

    building background

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { SunIcon } from "@heroicons/react/24/outline";
+
+function HomePage() {
+  return (
+    <div className="text-white flex flex-col items-center justify-center h-screen px-2">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+      <div>
+        <div>
+          <div>
+            <SunIcon />
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
