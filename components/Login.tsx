5584675cb8e6059a35914d2aefbb91b1f94156bd
nikita <nikita.parekh@unboxrobotics.com>
2023-06-21 22:31:10 +0530

    login screen and functionality

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+      <Image
+        src="https://links.papareact.com/2i6"
+        width={300}
+        height={300}
+        alt="Logo"
+      />
+      <button
+        className="text-white text-3xl font-bold animate-pulse"
+        onClick={() => signIn("google")}
+      >
+        Sign In to use ChatGPT
+      </button>
+    </div>
+  );
 };
